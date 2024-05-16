7648ef8ee069ae9c762fe3c5de549dc2b6d89fa2
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-05-16 08:47:23 +0000

    Remove old tests.

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(mopr)
-
-test_check("mopr")
--- tests/testthat/test-getOptions.R
+++
@@ -1,5 +0,0 @@
-context("getMinionOptions")
-
-test_that("command line options are parsed correctly", {
-  expect_equal(getMinionOptions(out1), c("findallsols", "noprintsols"))
-})
